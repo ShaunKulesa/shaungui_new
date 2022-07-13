1b6b92acaebd310a260848befe02bfc700e941e7
Shaun Kulesa <shaunkulesa@gmail.com>
2022-07-13 13:47:12 +0100

    tetris side movement

--- main.py
+++ main.py
@@ -1,58 +1,30 @@
-import time
-
 import shaungui
-import glfw
 
-window = shaungui.Window("Shaun's Window", 1500, 750)
+window = shaungui.Window("Tetris", 500, 600, background_colour=[240, 240, 240, 255])
 
-canvas = shaungui.Canvas(window, 500, 500)
-canvas.place(0, 0)
+canvas = shaungui.Canvas(window, 250, 500, border_width=0)
+canvas.place(125, 0)
 
-canvas.create_rectangle(1, 1, 100, 100, [255, 0, 0, 255], id="rectangle")
+canvas.create_rectangle(0, 0, 25, 25, [255, 0, 0, 255], id="rectangle1")
+canvas.create_rectangle(25, 0, 25, 25, [255, 0, 0, 255], id="rectangle2")
+canvas.create_rectangle(50, 0, 25, 25, [255, 0, 0, 255], id="rectangle3")
+canvas.create_rectangle(75, 0, 25, 25, [255, 0, 0, 255], id="rectangle4")
 
 def update():
-    dt = window.delta_time
-
-    # Close window when ESCAPE key was pressed at any moment
-    if window.input.key_pressed('ESCAPE'):
-        window.close()
-
-    # Move canvas when key is pressed (WASD)
-    if window.input.key_pressed('W'):
-        canvas.move("rectangle", 0, 100 * dt)
-    elif window.input.key_pressed('S'):
-        canvas.move("rectangle", 0, -100 * dt)
-
     if window.input.key_pressed('A'):
-        canvas.move("rectangle", -100 * dt, 0)
+        canvas.move("rectangle1", -25, 0)
+        canvas.move("rectangle2", -25, 0)
+        canvas.move("rectangle3", -25, 0)
+        canvas.move("rectangle4", -25, 0)
+
     elif window.input.key_pressed('D'):
-        canvas.move("rectangle", 100 * dt, 0)
+        canvas.move("rectangle1", 25, 0)
+        canvas.move("rectangle2", 25, 0)
+        canvas.move("rectangle3", 25, 0)
+        canvas.move("rectangle4", 25, 0)
 
-    # Update at same rate as renderer
-    window.after(update, 0)
+    window.after(update, 0.1)
 
-# Update at same rate as renderer
-window.after(update, 0)
-
-def escape():
-    window.close()
-
-def right():
-    canvas.move("rectangle", 100 * window.delta_time, 0)
-
-def left():
-    canvas.move("rectangle", -100 * window.delta_time, 0)
-
-def up():
-    canvas.move("rectangle", 0, 100 * window.delta_time)
-
-def down():
-    canvas.move("rectangle", 0, -100 * window.delta_time)
-
-window.key_bind("ESCAPE", escape)
-window.key_bind("D", right)
-window.key_bind("A", left)
-window.key_bind("W", up)
-window.key_bind("S", down)
+window.after(update, 0.1)
 
 shaungui.start()
--- shaungui/canvas/canvas.py
+++ shaungui/canvas/canvas.py
@@ -1,3 +1,4 @@
+from time import perf_counter
 from OpenGL import GL
 from .rectangle_drawer import RectangleDrawer
 
@@ -17,7 +18,7 @@ class Canvas:
         self.rectangle_drawer = RectangleDrawer(self.width, self.height)
 
         # background
-        self.create_rectangle(0, 0, self.width, self.height, [item / 255 for item in background_colour])
+        self.create_rectangle(0, 0, self.width, self.height, [item / 255 for item in background_colour], id="canvas_background")
 
         # border
         self.create_rectangle(0, 0, self.width, border_width, [item / 255 for item in border_colour], id="bottom_border")
@@ -30,17 +31,18 @@ class Canvas:
     def create_rectangle(self, x, y, width, height, colour, id=None, tags=[]):
         self.rectangle_drawer.rectangle_points.extend([x, y, width, height, colour[0], colour[1], colour[2], colour[3]])
         self.rectangle_drawer.rectangle_buffer_needs_updating = True
+        
+        if id is None:
+            id = f'canvas_rectangle{self.counter}'
+            self.ids[id] = ["rectangle", len(self.rectangle_drawer.rectangle_points) // 8]
+            self.counter += 1
+            return
 
         for tag in tags:
             if tag not in self.tags:
                 self.tags[tag] = []
-            self.tags[tag].append(self.counter)
-
-        if id is None:
-            self.ids[f'canvas_rectangle{self.counter}'] = ["rectangle", len(self.rectangle_drawer.rectangle_points) // 8]
-            self.counter += 1
-            return
-        
+            self.tags[tag].append(id)
+       
         self.ids[id] = ["rectangle", len(self.rectangle_drawer.rectangle_points) // 8]
     
     def place(self, x, y):
@@ -65,34 +67,74 @@ class Canvas:
         for shape_id in self.ids:
             if id == shape_id:
                 if self.ids[id][0] == "rectangle":
-                    return self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 8], self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 1) - 8]
+                    return self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 8], self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 7]
                 break
     
     def get_size(self, id) -> list[float]:
         for shape_id in self.ids:
             if id == shape_id:
                 if self.ids[id][0] == "rectangle":
-                    return self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 7], self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 1) - 7]
+                    return self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 6], self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 5]
                 break
     
     def get_colour(self, id) -> list[float]:
         for shape_id in self.ids:
             if id == shape_id:
                 if self.ids[id][0] == "rectangle":
-                    return self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 6], self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 1) - 6], self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 2) - 6], self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 3) - 6]
+                    return [
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 4],
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 3],
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 2],
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 1]
+                    ]
                 break
     
     def set_colour(self, id, colour):
         for shape_id in self.ids:
             if id == shape_id:
                 if self.ids[id][0] == "rectangle":
-                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 6] = colour[0]
-                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 1) - 6] = colour[1]
-                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 2) - 6] = colour[2]
-                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8 + 3) - 6] = colour[3]
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 4] = colour[0]
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 3] = colour[1]
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 2] = colour[2]
+                    self.rectangle_drawer.rectangle_points[(self.ids[id][1] * 8) - 1] = colour[3]
                     self.rectangle_buffer_needs_updating = True
                 break
     
+    def tag_collision(self, tag, target_id=None, target_tags=[]) -> list:
+        if target_id:
+            target = self.ids[target_id]
+            x, y = self.get_coords(target_id)
+            width, height = self.get_size(target_id)
+
+            # if sel.ids[target_id][0] == "rectangle":
+            #     return self.rectangle_drawer.rectangle_collision(x, y, width, height, tag, target_tags
+    
+    def id_collision(self, id, target_id=None, target_tag=None) -> list:
+        collisions = []
+        
+        if target_id:
+            if self.ids[id][0] == "rectangle" and self.ids[target_id][0] == "rectangle":
+                x, y = self.get_coords(id)
+                width, height = self.get_size(id)
+                target_x, target_y = self.get_coords(target_id)
+                target_width, target_height = self.get_size(target_id)
+
+                if x < target_x + target_width and x + width > target_x and y < target_y + target_height and y + height > target_y:
+                    collisions.append(target_id)
+
+        if target_tag:
+            for object in self.tags[target_tag]:
+                if self.ids[object][0] == "rectangle" and self.ids[id][0] == "rectangle":
+                    x, y, = self.get_coords(object)
+                    width, height = self.get_size(object)
+                    target_x, target_y = self.get_coords(id)
+                    target_width, target_height = self.get_size(id)
+
+                    if x < target_x + target_width and x + width > target_x and y < target_y + target_height and y + height > target_y:
+                        collisions.append(object)
+        
+        return collisions
+        
     def render(self):
         GL.glViewport(self.x, self.y, self.width, self.height)
 

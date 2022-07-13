70e9bd23c830c154153d393481e509d862ff8cc5
Shaun Kulesa <shaunkulesa@gmail.com>
2022-07-13 14:06:32 +0100

    tetris border

--- main.py
+++ main.py
@@ -12,16 +12,19 @@ canvas.create_rectangle(75, 0, 25, 25, [255, 0, 0, 255], id="rectangle4")
 
 def update():
     if window.input.key_pressed('A'):
-        canvas.move("rectangle1", -25, 0)
-        canvas.move("rectangle2", -25, 0)
-        canvas.move("rectangle3", -25, 0)
-        canvas.move("rectangle4", -25, 0)
+        if canvas.get_coords("rectangle1")[0] >= 25:
+            canvas.move("rectangle1", -25, 0)
+            canvas.move("rectangle2", -25, 0)
+            canvas.move("rectangle3", -25, 0)
+            canvas.move("rectangle4", -25, 0)
 
     elif window.input.key_pressed('D'):
-        canvas.move("rectangle1", 25, 0)
-        canvas.move("rectangle2", 25, 0)
-        canvas.move("rectangle3", 25, 0)
-        canvas.move("rectangle4", 25, 0)
+        if canvas.get_coords("rectangle4")[0] <= 200:
+            canvas.move("rectangle1", 25, 0)
+            canvas.move("rectangle2", 25, 0)
+            canvas.move("rectangle3", 25, 0)
+            canvas.move("rectangle4", 25, 0)
+
 
     window.after(update, 0.1)
 
